d94274e0c1b10ee5edf6ff98bd95adf6ea7ee2bb
philquake <phillipcole123@gmail.com>
2021-10-08 12:12:38 -0500

    commit on pyttsx3

--- main.py
+++ main.py
@@ -1,60 +1,32 @@
 #https://public-apis.io/evil-insult-generator-api
 #https://complimentr.com/
 #https://m3o.com/
+from typing import final
 import requests
 from bs4 import BeautifulSoup
-from gtts import gTTS
-import os
-from requests.structures import CaseInsensitiveDict
+import pyttsx3
+
+engine = pyttsx3.init() # object creation
 
 def insult():
     URL = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, 'html5lib')
     soup.get_text()
-    # print(soup.get_text())
-    # print(soup.get_text().split('insult":"'))
     splitword = 'insult":"'
     initalsplit = soup.get_text().partition(splitword)[2] #splits extra text before insult
     finalsplit = initalsplit.rpartition('","created"')[0] #splits extra text after insult
-    tts(finalsplit)
+    speech(finalsplit)
 
-def compliment():
-    URL = "https://complimentr.com/api"
-    r = requests.get(URL)   
-    soup = BeautifulSoup(r.content, 'html5lib')
-    soup.get_text()
-    splitword = 'compliment":"'
-    initalsplit = soup.get_text().partition(splitword)[2] #splits extra text before insult
-    finalsplit = initalsplit.rpartition('"}')[0] #splits extra text after insult
-    tts(finalsplit)
+def speech(finalsplit):
+    print('this')
+    engine.say(finalsplit)
+    engine.runAndWait()
 
-def tts(finalsplit):
-    language = 'en'
-    myobj = gTTS(text=finalsplit, lang=language, slow=False)
-    myobj.save("audio.mp3")
-    os.system("start audio.mp3")
+def main ():
+    insult()
 
-def current_weather():
-    url = "https://api.m3o.com/v1/weather/Forecast"
-    headers = CaseInsensitiveDict()
-    headers["Content-Type"] = "application/json"
-    headers["Authorization"] = "Bearer MzdjNDlmZmUtNGMxOC00MGI2LWE3NTEtNDQwYTYzNWI3YzRl"
+main
 
-    data = """    {    "location": "Jamaica"    }    """
 
-    r = requests.post(url, headers=headers, data=data)
-    soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-    print(soup.prettify())
-    splitword = 'max_temp_f":'
-    asplit = soup.get_text().partition(splitword)[2] #splits extra text before insult
-    tempf = asplit.rpartition(',"min_temp_c')[0]
-    print(tempf)
 
-def main():
-    # insult()
-    # compliment()
-    current_weather()
-    
-
-main()
